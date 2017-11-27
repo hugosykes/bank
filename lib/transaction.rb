160d034f9e71154cac7ecaeefc22d9f910249543
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-27 15:01:29 +0000

    Adds functionality in the transaction class for creating a string of that transaction's activity

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,3 +1,16 @@
 class Transaction
+  def initialize(amount, balance)
+    amount >= 0 ? (@credit = amount) : (@debit = amount)
+    @balance = balance
+  end
 
+  def to_string
+    "#{Time.now.strftime('%d/%m/%Y')} || #{amount_string(@credit)}|| #{amount_string(@debit)}|| £#{format('%.2f', @balance)}"
+  end
+
+  private
+
+  def amount_string(amount = 0)
+    amount ? "£#{format('%.2f', amount)} " : ''
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,1 +1,15 @@
 require 'transaction'
+
+describe 'Transaction' do
+  let(:subject) { Transaction.new(100, 100) }
+
+  describe 'to_string' do
+    it 'should respond to the to_string method' do
+      expect(subject).to respond_to(:to_string)
+    end
+
+    it 'should return a row in a bank statement' do
+      expect(subject.to_string).to eq '27/11/2017 || £100.00 || || £100.00'
+    end
+  end
+end
