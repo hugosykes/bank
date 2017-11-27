b41808bd40e29fc5518a13babd51e82fab8b2882
hugosykes <hugo@denmeadfarm.co.uk>
2017-11-27 12:13:36 +0000

    Adds functionality to deposit and withdraw amounts. Raises errors if amounts are negative

--- lib/account.rb
+++ lib/account.rb
@@ -4,4 +4,12 @@ class Account
   def initialize
     @balance = 0
   end
+
+  def deposit(amount)
+    amount >= 0 ? @balance += amount : raise("Can't deposit negative amounts!")
+  end
+
+  def withdraw(amount)
+    amount >= 0 ? @balance -= amount : raise("Can't withdraw negative amounts!")
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,4 +10,32 @@ describe 'Account' do
     end
   end
 
+  describe 'deposit' do
+    it 'should respond to deposit method' do
+      expect(subject).to respond_to(:deposit)
+    end
+
+    it 'should change balance by 100 when 100 is deposited' do
+      expect{subject.deposit(100)}.to change {subject.balance}.by(100)
+    end
+
+    it 'should raise an error when trying to deposit a negative amount' do
+      expect{subject.deposit(-100)}.to raise_error("Can't deposit negative amounts!")
+    end
+  end
+
+  describe 'withdraw' do
+    it 'should respond to the withdraw method' do
+      expect(subject).to respond_to(:withdraw)
+    end
+
+    it 'should change balance by -100 when 100 is withdrawn' do
+      expect{subject.withdraw(100)}.to change {subject.balance}.by(-100)
+    end
+
+    it 'should raise an error when trying to withdraw a negative amount' do
+      expect{subject.withdraw(-100)}.to raise_error("Can't withdraw negative amounts!")
+    end
+  end
+
 end
